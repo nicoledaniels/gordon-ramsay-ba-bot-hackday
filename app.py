6ec49f9c7380e3d42f08c4dd0ac4f81029186b67
Nicole Daniels <nicole.daniels@blueapron.com>
2017-08-25 13:37:26 -0400

    Ignore images

--- app.py
+++ app.py
@@ -38,7 +38,10 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    try
+                        message_text = messaging_event["message"]["text"]  # the message's text
+                    except Exception as inst:
+                        return "ok",200
 
                     response_message = response.classify(message_text)
 
