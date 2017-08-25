3386b3c7f2ab3f6060bfb6f3d955be25e984eb3f
Nicole Daniels <nicole.daniels@blueapron.com>
2017-08-25 13:14:35 -0400

    Change search for matching words

--- response.py
+++ response.py
@@ -1,11 +1,12 @@
 import os
 import sys
 
-drink = (["drink","beverage","liquid"])
+drink = ["drink","beverage","liquid"]
 
 def classify(msg):
     msg = msg.strip()
     msg = msg.lower()
     
-    if(drink in msg):
-       return "https://media.giphy.com/media/ZXW1OXBCTRF2o/giphy.gif"
+    for word in drink:
+      if word in msg:
+        return "https://media.giphy.com/media/ZXW1OXBCTRF2o/giphy.gif"
